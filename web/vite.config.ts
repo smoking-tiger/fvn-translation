7402b12913df16632e9ba861cd7d8efa4c328683
nukbal <frostwolfclaw@gmail.com>
2025-03-29 16:35:46 +0900

    Squashed commit of the following:
    commit ffffb5605975febd26e4f375366c7233c996fd40
    Author: Dorurung <james580309@gmail.com>
    Date:   Sat Mar 22 17:11:46 2025 +0900
    
        fix: Change base of vite
    
    commit 69ecf7732e2282421372756ba0a1a29080092ebb
    Author: Dorurung <james580309@gmail.com>
    Date:   Sat Mar 22 17:06:10 2025 +0900
    
        Add webpage

---
+++ web/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
+      }
+    }
+  },
+  base: '/fvn-translation'
+})
