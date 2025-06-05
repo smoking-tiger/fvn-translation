7ab8727ab8ded4096a19c0818fe1c61be47c8b12
nukbal <frostwolfclaw@gmail.com>
2025-06-05 22:44:27 +0900

    fix: add xml definition

--- react-router.config.ts
+++ react-router.config.ts
@@ -20,7 +20,7 @@ export default {
   appDirectory: 'src',
   prerender: list,
   async buildEnd(args) {
-    const arr = ['<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'];
+    const arr = ['<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'];
     let latest: Date | null = null;
 
     for await (const name of list.reverse()) {
