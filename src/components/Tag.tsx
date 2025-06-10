4949da310541752c02dc8875c626a82752f79cea
nukbal <frostwolfclaw@gmail.com>
2025-06-10 23:02:28 +0900

    fix: 18 tag more visible on light mode

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -10,8 +10,8 @@ export default function Tag({ name, className, ...rest }: Props) {
     'px-2 py-1 rounded-lg border-1',
     className,
     {
-      'bg-red-50 dark:bg-red-700 border-red-200 dark:border-red-800': name === '18금',
-      'bg-stone-50 dark:bg-stone-700 border-stone-200 dark:border-stone-800': name !== '18금'
+      'bg-red-100 dark:bg-red-700 border-red-500 dark:border-red-800': name === '18금',
+      'bg-stone-50 dark:bg-stone-700 border-stone-200 dark:border-stone-800': name !== '18금',
     },
   ), [className, name]);
   return (
