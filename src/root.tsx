f28f586748862d4d85202efe06577da51007be16
nukbal <frostwolfclaw@gmail.com>
2025-07-06 22:35:14 +0900

    fix: add discord on footer

--- src/root.tsx
+++ src/root.tsx
@@ -3,6 +3,7 @@ import { useLayoutEffect } from 'react';
 import { Links, Meta, Outlet, Scripts, ScrollRestoration, isRouteErrorResponse } from 'react-router';
 
 import Header from './components/Header';
+import IconDiscord from './components/Icons/Discord';
 import { useThemeMode, uiStore, THEME_STORAGE_KEY, COLOR_SCHEME_QUERY, IS_SSR } from './ui';
 import type { Route } from "./+types/root";
 
@@ -67,12 +68,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="googlebot" content="index,follow" />
         <Links />
       </head>
-      <body className="relative min-h-screen pb-5">
+      <body className="relative min-h-screen pb-12">
         <Header />
         {children}
-        <footer className="absolute sticky bottom-0">
-          <div className="container p-2 m-auto">
+        <footer className="fixed bottom-0 left-0 right-0 z-20 w-full p-4">
+          <div className="container p-2 m-auto flex items-center justify-between">
             <small className="opacity-55">모든 번역의 권리는 원 게임 제작자(혹은 제작사)에게 있습니다. 털겜번역단은 비공식 팬 번역을 하는 팀입니다.</small>
+            <div className="flex mt-4 sm:justify-center sm:mt-0">
+              <a href="https://discord.com/invite/U7XsRUc83T" className="ms-5">
+                <IconDiscord className="w-4 h-4" />
+                <span className="sr-only">털겜번역단 디스코드</span>
+              </a>
+            </div>
           </div>
         </footer>
         <ScrollRestoration />
