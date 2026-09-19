a0c99a51930ce48261ab426c13554d692908d5cd
nukbal <frostwolfclaw@gmail.com>
2026-09-19 19:19:37 +0900

    Password r7

--- src/metadata.ts
+++ src/metadata.ts
@@ -53,6 +53,7 @@ export const member = {
 export const license = {
   '나눔폰트': 'https://help.naver.com/service/30016/contents/18088?osType=PC&lang=ko',
   '프리텐다드': 'https://github.com/orioncactus/pretendard/blob/main/LICENSE',
+  '함렡': 'https://github.com/hyper-type/hahmlet/blob/master/OFL.txt',
   '리디바탕체': 'https://ridicorp.com/ridibatang/',
   '인천교육힘찬체': '',
   '제주한라산': '',
